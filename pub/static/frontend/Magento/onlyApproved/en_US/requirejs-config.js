a360252b84bf37af66bc45d95d05a761f12fdae9
IYadu07 <yadu.krishnan@yatnam.com>
2024-01-06 19:09:36 +0530

    footer and main content update jan 6

--- pub/static/frontend/Magento/onlyApproved/en_US/requirejs-config.js
+++ pub/static/frontend/Magento/onlyApproved/en_US/requirejs-config.js
@@ -1108,13 +1108,58 @@ require.config(config);
  */
 
 var config = {
-    shim: {
-        'onlyApproved/javascript/javascript': {
-            deps: ['Magento_Customer/js/invalidation-rules/website-rule'],
-            exports: 'javascript'
+    map: {
+        '*': {
+            'custom-script': 'Magento_onlyApproved/js/javascript'
         }
     }
 };
+
+
+
+// var config = {
+//     map: {
+//         '*': {
+//             alias: 'Vendor_Module/js/complex/path/amd-module'
+//         },
+//         'Vendor_Module/js/amd-module': {
+//             alias_two: 'Vendor_Module/js/complex/path/amd-module-two'
+//         }
+//     },
+
+//     paths: {
+//         'alias': 'library/file',
+//         'another-alias': 'https://some-library.com/file'
+//     }
+    
+    // paths: {
+
+    // },
+    // deps: [...],
+    // shim: {...},
+    // config: {
+    //     mixins: {...},
+    //     text: {...}
+    // }
+// }
+
+
+// working
+
+// var config = {
+//     deps: [
+//         'javascript/javascript'
+//     ],
+// };
+
+// var config = {
+//     shim: {
+//         'onlyApproved/javascript/javascript': {
+//             deps: ['Magento_Customer/js/invalidation-rules/website-rule'],
+//             exports: 'javascript'
+//         }
+//     }
+// };
 
 require.config(config);
 })();
