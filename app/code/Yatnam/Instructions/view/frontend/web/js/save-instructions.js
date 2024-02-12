05e4fc532af408df720129522139756129c87009
IYadu07 <yadu.krishnan@yatnam.com>
2024-02-12 18:31:29 +0530

    observer and controller update Feb 12

--- app/code/Yatnam/Instructions/view/frontend/web/js/save-instructions.js
+++ app/code/Yatnam/Instructions/view/frontend/web/js/save-instructions.js
@@ -2,21 +2,17 @@ define(["jquery"], function ($) {
     "use strict";
 
     $(document).ready(function () {
-        console.log("working until now");
-
         $(document).on("click", ".action.primary.checkout", function (event) {
             event.preventDefault();
             var deliveryInstructions = $(".admin__control-textarea").val();
-            console.log(deliveryInstructions);
             $("#delivery_instructions_display").text(deliveryInstructions);
             $.ajax({
-                url: '/yatnam/instructions/index/savedeliveryinstructions',
+                url: "/OnlyApproved/Instructions/Index/Index",
                 type: "POST",
                 dataType: "json",
-                data: JSON.stringify({
+                data: {
                     delivery_instructions: deliveryInstructions,
-                }),
-                contentType: "application/json",
+                },
                 success: function (response) {
                     if (response.success) {
                         console.log(
@@ -36,32 +32,3 @@ define(["jquery"], function ($) {
         });
     });
 });
-
-
-// $('#co-shipping-form').submit(function(event){
-//     console.log("Form submitted!");
-// event.preventDefault();
-
-// var deliveryInstructions = $('#delivery_instructions').val();
-// console.log(deliveryInstructions);
-
-// $('#delivery_instructions_display').text(deliveryInstructions);
-
-// $.ajax({
-//     url: '/Yatnam/Instructions/Observer/SaveDeliveryInstructionsObserver.php',
-//     type: 'POST',
-//     dataType: 'json',
-//     data: JSON.stringify({ delivery_instructions: deliveryInstructions }), // Stringify the data
-//     contentType: 'application/json', // Set content type to JSON
-//     success: function(response) {
-//         if (response.success) {
-//             console.log('Delivery instructions saved successfully.');
-//         } else {
-//             console.error('Failed to save delivery instructions:', response.error);
-//         }
-//     },
-//     error: function(xhr, status, error) {
-//         console.error('AJAX request failed:', error);
-//     }
-// });
-// });
